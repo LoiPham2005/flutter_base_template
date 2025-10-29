a943f9bfcec752ed77f6d1202414bf3cd5833b21
đức lợi phạm <phamducloi919@gmail.com>
2025-10-29 09:31:34 +0700

    update

--- lib/features/auth/domain/usecases/logout_usecase.dart
+++ lib/features/auth/domain/usecases/logout_usecase.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_base_template/core/errors/result.dart';
 import 'package:injectable/injectable.dart';
-import '../entities/auth_entity.dart';
 import '../repositories/auth_repository.dart';
 
 @injectable
