8028b4e750480f6a858a6bd1620bff5aee331b8d
đức lợi phạm <phamducloi919@gmail.com>
2025-11-08 20:03:50 +0700

    refactor: Revise project structure and configurations for environment flavors and launch settings

--- lib/main.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_base_template/core/config/environment_config.dart';
-import 'package:flutter_base_template/main_common.dart';
-
-void main() => mainCommon(Environment.production);
